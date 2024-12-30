8dc7447b8dc15575b53287442831321e44c259d5
Neeraj Kumar <neerajsohal@users.noreply.github.com>
2024-12-30 13:45:08 +0530

    refactor: checked and finalised logout command

--- src/commands/logout.js
+++ src/commands/logout.js
@@ -1,6 +1,8 @@
 const { clearToken } = require('../utils/config');
+const { baseCommand } = require('./baseCommand');
 
-const logout = async () => {
+const logout = async (options) => {
+    await baseCommand(options); // This will set debug mode if -d or --debug is provided
     clearToken();
 };
 
